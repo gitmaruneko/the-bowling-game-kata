41608617c214ad97073cd6d68c66d1e7fb974550
Raouf Aghrout <aghrout.raouf@gmail.com>
2015-08-12 16:06:10 +0100

    added first test

--- src/main/java/com/butunclebob/BowlingGame.java
+++ src/main/java/com/butunclebob/BowlingGame.java
@@ -2,4 +2,11 @@ package com.butunclebob;
 
 public class BowlingGame {
 
+    public void roll(int pins) {
+
+    }
+
+    public int getScore() {
+        return 0;
+    }
 }
--- src/test/java/com/butunclebob/BowlingGameTest.java
+++ src/test/java/com/butunclebob/BowlingGameTest.java
@@ -1,6 +1,20 @@
 package com.butunclebob;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class BowlingGameTest {
 
-    
+    @Test
+    public void shouldScoreWhenAllRollsGuttered() {
+        BowlingGame bowlingGame = new BowlingGame();
+
+        for (int i = 0; i < 20; i++) {
+            bowlingGame.roll(0);
+        }
+
+        assertThat(bowlingGame.getScore(), is(0));
+    }
 }
