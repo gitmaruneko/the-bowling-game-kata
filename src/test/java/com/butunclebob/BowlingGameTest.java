65c2a108b838392320812c2f6bad4469d18e5ac5
Raouf Aghrout <aghrout.raouf@gmail.com>
2015-08-12 16:44:42 +0100

    added third test

--- src/test/java/com/butunclebob/BowlingGameTest.java
+++ src/test/java/com/butunclebob/BowlingGameTest.java
@@ -1,6 +1,7 @@
 package com.butunclebob;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,22 +17,36 @@ public class BowlingGameTest {
     }
 
     @Test
-    public void shouldScoreWhenAllRollsGutter() {
+    public void shouldScoreGutterGame() {
         rollMany(20, 0);
 
         assertThat(bowlingGame.getScore(), is(0));
     }
 
     @Test
-    public void shouldScoreWhenAllRollsOne() {
+    public void shouldScoreAllOnes() {
         rollMany(20, 1);
 
         assertThat(bowlingGame.getScore(), is(20));
+    }
+
+    @Test
+    public void shouldScoreOneSpare() {
+        rollSpare();
+        bowlingGame.roll(3);
+        rollMany(17, 0);
+
+        assertThat(bowlingGame.getScore(), is(16));
     }
 
     private void rollMany(int rolls, int pins) {
         for (int i = 0; i < rolls; i++) {
             bowlingGame.roll(pins);
         }
+    }
+
+    private void rollSpare() {
+        bowlingGame.roll(5);
+        bowlingGame.roll(5);
     }
 }
